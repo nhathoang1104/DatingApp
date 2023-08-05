d72c99848732d1c53912731b362c474fe8eb7c61
Hoang11 <nhathoang69123@gmail.com>
2023-08-05 14:07:46 +0700

    Het chapter 5

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,7 +10,7 @@ namespace API.Extensions
     public static class ApplicationServiceExtensions
     {   
         public static IServiceCollection AddApplicationServices(this IServiceCollection services , IConfiguration config){
-               services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
